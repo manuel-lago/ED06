c30ec95952ffffa44def48ad3667aaf01f69db02
manuel-lago <manuel.lago.fdz@gmail.com>
2024-05-08 21:21:53 +0200

    4. Encapsular los atributos de la clase CCuenta.

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -19,16 +19,72 @@ public class CCuenta {
         cuenta = cue;
         saldo = sal;
     }
+    
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
 
-    public double estado() {
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
         return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInterés
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * @param tipoInterés the tipoInterés to set
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+    
+    public double estado() {
+        return getSaldo();
     }
 
     public void ingresar(double cantidad) throws Exception {
         if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
         }
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
     public void retirar(double cantidad) throws Exception {
@@ -38,6 +94,6 @@ public class CCuenta {
         if (estado() < cantidad) {
             throw new Exception("No se hay suficiente saldo");
         }
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
 }
