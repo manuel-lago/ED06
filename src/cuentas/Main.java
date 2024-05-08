2f7b7ddb9ff40605ae459313b2a29bab37693f35
manuel-lago <manuel.lago.fdz@gmail.com>
2024-05-08 21:23:11 +0200

    5. Añadir un nuevo parámetro al método operativa_cuenta, de nombre cantidad y de tipo float.

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -12,11 +12,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        operativa_cuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        operativa_cuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0, 2000);
 
     }
 
-    static void operativa_cuenta(String nombre, String cuenta, double saldo, double tipo) {
+    static void operativa_cuenta(String nombre, String cuenta, double saldo, double tipo, float cantidad) {
         CCuenta cuenta1;
         double saldoActual;
 
@@ -25,13 +25,13 @@ public class Main {
         System.out.println("El saldo actual es " + saldoActual);
 
         try {
-            cuenta1.retirar(2300);
+            cuenta1.retirar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(695);
+            cuenta1.ingresar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
